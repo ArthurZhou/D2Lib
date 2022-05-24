8a2a0b0b20c4f2611fd6292877c5bae76a79b2d3
ArthurZhou <89689293+ArthurZhou@users.noreply.github.com>
2022-05-24 13:02:37 +0800

    API login example

---
+++ api.py
@@ -0,0 +1,9 @@
+import http.client
+
+connection = http.client.HTTPConnection("0.0.0.0", 80)
+connection.request("GET", "/", headers={"API-Auth": "root:root"})
+response = connection.getresponse()
+print("Status: {} and reason: {}".format(response.status, response.reason))
+print(response.read().decode())
+
+connection.close()
