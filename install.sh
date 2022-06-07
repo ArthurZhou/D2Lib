647199f314b917e3c9f402b67ebcdc7c947b7a19
ArthurZhou <89689293+ArthurZhou@users.noreply.github.com>
2022-06-07 09:59:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,4 @@
 echo Feching source code...
-curl https://raw.githubusercontent.com/ArthurZhou/D2Lib/main/d2lib.py --output d2lib.py
-echo Making libraries...
-mkdir d2lib
-echo Making example files...
-cd d2lib
-curl https://raw.githubusercontent.com/ArthurZhou/D2Lib/main/Home.md --output Home.md
-curl https://raw.githubusercontent.com/ArthurZhou/D2Lib/main/404.md --output 404.md
-cd ..
-echo Finish! Use 'python3 d2lib.py' to fire it up!
+curl https://raw.githubusercontent.com/ArthurZhou/D2Lib/main/D2Lib.py --output D2Lib.py
+echo Finish! Use 'python3 D2Lib.py' to fire it up!
 echo Problems? Check out our Wiki -> https://github.com/ArthurZhou/D2Lib/wiki
